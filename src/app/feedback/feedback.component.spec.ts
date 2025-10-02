24f8e678808dbdfff1b7699159b77419ea2d3b46
MikMakQT <122343662+MikMakQT@users.noreply.github.com>
2025-10-02 10:05:37 +0300

    Cancelbuttonnavigationtest2.10

--- src/app/feedback/feedback.component.spec.ts
+++ src/app/feedback/feedback.component.spec.ts
@@ -1,28 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter, Router } from '@angular/router';
+import { provideLocationMocks } from '@angular/common/testing';
 import { FeedbackComponent } from './feedback.component';
+import { HomeComponent } from '../home/home.component';
 //import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
 
+  let router: Router;
+  let routerSpy: jasmine.Spy;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FeedbackComponent, BrowserAnimationsModule],
+      providers: [provideRouter([{ path: 'home', component: HomeComponent }]),
+      provideLocationMocks(),
+      ],
       /*schemas: [
         CUSTOM_ELEMENTS_SCHEMA,
         NO_ERRORS_SCHEMA
       ]*/
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(FeedbackComponent);
     component = fixture.componentInstance;
+
+    router = TestBed.inject(Router);
+    routerSpy = spyOn(router, 'navigate');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('cancel navigates to homepage', () => {
+    console.log('qwerty123456');
+    //component.cancel();
+
+    //expect(routerSpy).toHaveBeenCalledWith(['home']);
   });
 });
